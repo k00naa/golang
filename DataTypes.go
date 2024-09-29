b5481da7519624b9784166d731dea58e592e1ee6
k00naa <itssayedahmed7@gmail.com>
2024-09-29 11:29:18 -0700

    first commit

---
+++ DataTypes.go
@@ -0,0 +1,12 @@
+package main 
+
+import ("fmt")
+
+func main() {
+	var a bool = false
+	var b int = 55
+	var c float32 = 4.7
+	var d string = "Kona"
+
+	fmt.Println(a, b, c, d)
+}
---
+++ helloWorld.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myName string = "Kona"
+	var MyAge int = 100
+	var myMonth float32 = 44.45
+	var female bool = true
+	fmt.Println("hello world", myName, MyAge, myMonth, female)
+}
